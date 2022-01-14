9965741f1bb1c43cfd9b198991258f46db4f658b
HAsNaa AbdelRahman <83743822+ErrOrrrrrrrrrrrrrrrr@users.noreply.github.com>
2022-01-14 20:44:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+char s1[123] , s2[123] ;
+int main() 
+{
+	  ios_base::sync_with_stdio(false); 
+	  cin.tie(NULL); cout.tie(NULL);
+cin >> s1 >> s2 ;
+cout << stricmp(s1 , s2);
+
+return 0;
+}
+

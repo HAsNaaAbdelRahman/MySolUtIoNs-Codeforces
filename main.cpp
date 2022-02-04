e8e7733ac27bf28ac05371cd5f76498dae9f149b
HAsNaa AbdelRahman <83743822+ErrOrrrrrrrrrrrrrrrr@users.noreply.github.com>
2022-02-04 20:43:11 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() 
+{
+	// cout << fixed << setprecision(6)<< endl;
+	  ios_base::sync_with_stdio(false); 
+	  cin.tie(NULL); cout.tie(NULL);
+            
+            double  x ;                         cin >> x ;
+            cout << ceil(x / 5);
+              
+return 0;
+}
+
